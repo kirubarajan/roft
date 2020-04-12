1e914edf4170eb3f180062a68798c929fb06703e
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-12 15:32:53 -0400

    Add initial flask app

---
+++ main.py
@@ -0,0 +1,11 @@
+from data import Dataset
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    dataset = Dataset("sample.txt")
+    return render_template("index.html", sentences=dataset.sentences)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
