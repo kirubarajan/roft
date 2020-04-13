092a1c04fa3ce3478f22f766f46ce0b480175cca
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-13 14:58:16 -0400

    Fix parsing bug

--- data.py
+++ data.py
@@ -4,4 +4,4 @@ class Dataset:
     def __init__(self, path):
         with open(path) as data:
             self.text = data.read()
-            self.sentences = [sentence.strip() for sentence in self.text.split(".")]
+            self.sentences = [sentence.strip() for sentence in self.text.split(".") if sentence.strip() != ""]
