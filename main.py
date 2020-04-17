32ff25ad063207032f717fdff74fb268dfbe375e
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-17 19:13:38 -0400

    Add user capabilities and modal

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from data import Dataset
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def onboard():
+    return render_template("onboard.html")
+
+@app.route("/annotate")
+def annotate():
+    name = request.args.get('name')
     dataset = Dataset("sample.txt")
     dataset.prompt = ""
-    return render_template("index.html", prompt=dataset.sentences[0], sentences=dataset.sentences[1:])
+    return render_template("annotate.html", prompt=dataset.sentences[0], sentences=dataset.sentences[1:])
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
