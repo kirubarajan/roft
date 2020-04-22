0b17d24fdf9c3786ab7931ebda359450dc7e7829
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-22 14:03:01 -0400

    Delete generate_data.sh

--- generate_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: Generate a random number
-
-# Read in from one of the various prompt files a prompt of size length = to the generated random number
-
-# now call python3 data/gpt-2/src/interactive_conditional_samples.py and save output
-
-# Cat original prompt, <<BOUNDARY>> and the generation into its own file
