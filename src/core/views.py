46fc4096b7e7a2b071c02e00ba0fcd4035fa4d76
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-04-22 14:02:23 -0400

    Add better annotation flow

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,4 @@
+import ast
 import json
 import random
 from django.shortcuts import render, redirect
@@ -5,20 +6,26 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Prompt, Tag, EvaluationText, Annotation
 
-MAX_SENTENCES = 5
-
 def onboard(request):
     return render(request, "onboard.html", {})
 
 def annotate(request):
     text = random.choice(EvaluationText.objects.all())
-    sentences = text.body.split(".")
-    args = {"prompt": text.prompt, "text_id": text.pk, "sentences": json.dumps(sentences), "name": request.GET['name'], "max_sentences": MAX_SENTENCES}
+    sentences = ast.literal_eval(text.body)
+    args = {
+        "prompt": text.prompt, 
+        "text_id": text.pk, 
+        "sentences": json.dumps(sentences), 
+        "name": request.GET['name'], 
+        "max_sentences": len(sentences),
+        "boundary": text.boundary
+    }
+    
     return render(request, "annotate.html", args)
 
 @csrf_exempt
 def save(request):  
-    text = int(request.POST['text'][0])
+    text = int(request.POST['text'])
     name = request.POST['name']
     boundary = int(request.POST['boundary'])
     note = request.POST['note']
