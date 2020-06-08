79164c3a4d0538c7e01f0e425684136c3898b58e
Liam Dugan <ldugan@seas.upenn.edu>
2020-06-08 19:00:15 -0400

    Changed NYT process to output one article per line, updated generations, added Reddit writing prompts processing script

---
+++ generation/Reddit-Stories/process-Reddit.py
@@ -0,0 +1,43 @@
+'''
+    Script to parse out the raw text of articles from the Reddit Writing Prompts Corpus
+
+    This script will look for a directory named raw and find any .wp_target
+    files inside, detokenize it, strip all newlines and carriage returns from
+    the raw text and then write the text out, one story per line
+    to two files in an 80/20 split named "reddit-stories-train.txt" and
+    "reddit-stories-test.txt"
+'''
+
+import os, random
+from sacremoses import MosesDetokenizer
+import xml.etree.ElementTree as xml
+
+reddit_corpus_file_location = './raw'
+pretraining_output_file_path = './reddit-stories-train.txt'
+sampling_output_file_path = './reddit-stories-test.txt'
+
+def clean(text):
+  tokens = text.split(' ')
+  tokens = [t.replace('<newline>', '\n').replace('``', '"').replace("''", '"') for t in tokens]
+  detoked_string = ''.join(MosesDetokenizer().detokenize(tokens)).replace('\n', ' ').replace('\r', '') + '\n'
+  return detoked_string
+
+def determine_outfile(filename):
+    if 'train' in filename:
+        return pretraining_output_file_path
+    else:
+        return sampling_output_file_path
+
+if __name__ == '__main__':
+    if os.path.exists(reddit_corpus_file_location) and os.path.isdir(reddit_corpus_file_location):
+        for index, filename in enumerate(os.listdir(reddit_corpus_file_location)):
+            if filename.endswith('.wp_target'):
+                print("Processing " + filename)
+                path = os.path.join(reddit_corpus_file_location, filename)
+                outfile = determine_outfile(filename)
+
+                with open(path, 'r+') as f:
+                    with open(outfile, 'w+') as out_f:
+                        for index, line in enumerate(f):
+                            out_f.write(clean(line))
+                            print('Wrote Story #{0} of file {1}'.format(str(index), filename))
