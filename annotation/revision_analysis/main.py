bed71745619608ec66b542105055bcedac9faa76
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-07-26 19:25:17 -0400

    Update word frequency analysis

--- annotation/revision_analysis/main.py
+++ annotation/revision_analysis/main.py
@@ -2,29 +2,37 @@ import json
 from collections import defaultdict
 from config import stop_words, disallowed_revisions
 
-FREQUENCY_THRESHOLD = 50
+WORD_FREQUENCY_THRESHOLD = 40
+
 DB_FILE = 'db.json'
 OUTPUT_FILE = 'revisions.txt'
 
 with open(DB_FILE) as f:
     content = json.load(f)
 
+revisions = set()
 db_models = set()
-revisions = []
-freq = defaultdict(int)
+revision_freq = defaultdict(int)
+word_freq = defaultdict(int)
 
 for instance in content:
     db_models.add(instance['model'])
     if instance['model'] == 'core.annotation':
         revision = instance['fields']['revision'].strip()
+        revision_freq[revision] += 1
         if revision not in disallowed_revisions:
-            revisions.append(revision)
+            revisions.add(revision)
             for word in revision.split(' '):
-                freq[word.lower().strip()] += 1
+                word_freq[word.lower().strip()] += 1
 
-cleaned = [x for x in sorted(freq.items(), key=lambda x: x[1]) if (
-    x[1] > FREQUENCY_THRESHOLD and len(x[0]) and x[0] not in stop_words
+cleaned_word_freq = [x for x in sorted(word_freq.items(), key=lambda x: x[1]) if (
+    x[1] > WORD_FREQUENCY_THRESHOLD and len(x[0]) and x[0] not in stop_words
 )]
+
+cleaned_revision_freq = [x for x in sorted(revision_freq.items(), key=lambda x: x[1])]
+
+print(cleaned_word_freq)
+print(cleaned_revision_freq)
 
 with open(OUTPUT_FILE, 'w') as f:
     f.write("\n".join(revisions))
