262f300e0d6c404422d73c11afe60c6c212d60bb
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-08-01 20:00:21 -0400

    Refactor secret and reset

--- annotation/trick/settings.py
+++ annotation/trick/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h*^q$9-sp7bal(^gqx%5^u_&z)blwi*@nl#n%4%02md1*=vnxk'
+SECRET_KEY = os.getenv('SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
