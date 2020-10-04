939d1a646aa424c0fb04871175b316d0ba89e048
Arun Kirubarajan <kiruba@cis.upenn.edu>
2020-10-04 16:55:03 -0400

    Update new models

--- annotation/core/models.py
+++ annotation/core/models.py
@@ -12,13 +12,11 @@ class Profile(models.Model):
         return self.user.username + " " + str(self.is_turker)
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    text = models.CharField(max_length=100)
-    human = models.BooleanField()
-
-    def __str__(self):
-        return self.text
+class System(models.Model):
+    """The model that generated a given Generation."""
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    dataset_origin = models.URLField(null=True)
 
 
 class Prompt(models.Model):
@@ -29,11 +27,14 @@ class Prompt(models.Model):
         return self.body
 
 
-class EvaluationText(models.Model):
+class Generation(models.Model):
     """The continuation associated with the prompt."""
+    system = models.ForeignKey(System, on_delete=models.DO_NOTHING)
     prompt = models.ForeignKey(Prompt, on_delete=models.DO_NOTHING)
     body = models.TextField()
     boundary = models.IntegerField()
+    decoding_strategy = models.CharField(max_length=100, null=True)
+    decoding_strategy_value = models.FloatField(null=True)
 
     def __str__(self):
         return self.body
@@ -41,28 +42,34 @@ class EvaluationText(models.Model):
 
 class Annotation(models.Model):
     """A human annotation of a prompt-continuation pair."""
-    timestamp = models.DateTimeField(auto_now=True, null=True)
     annotator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    text = models.ForeignKey(EvaluationText, on_delete=models.DO_NOTHING)
+    timestamp = models.DateTimeField(auto_now=True, null=True)
+    generation = models.ForeignKey(Generation, on_delete=models.DO_NOTHING)
     boundary = models.IntegerField()
-    tags = models.ManyToManyField(Tag, related_name="annotation_tags")
-    revision = models.TextField()
     points = models.IntegerField()
+    revision = models.TextField()
     attention_check = models.BooleanField(default=False)
 
     def __str__(self):
         return self.annotator.username + " " + str(self.timestamp)
 
 
-class Group(models.Model):
+class Playlist(models.Model):
     """A grouping of several prompt-continuation pairs."""
     name = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
-    evaluation_texts = models.ManyToManyField(EvaluationText)
+    prompts = models.ManyToManyField(Prompt)
     version = models.FloatField(null=True)
-    decoding_strategy = models.CharField(max_length=100, null=True)
-    decoding_strategy_value = models.FloatField(null=True)
-    dataset_origin = models.URLField(null=True)
 
     def __str__(self):
         return self.name
+
+
+# deprecated
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    human = models.BooleanField()
+
+    def __str__(self):
+        return self.text
