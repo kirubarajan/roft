249eb085bee51849e3de1f568f248e58c380c5bb
liamdugan <ldugan@seas.upenn.edu>
2021-04-08 16:34:49 -0400

    Refactored Roft generator

--- generation/inference_scripts/roft_gpt2_generator.py
+++ generation/inference_scripts/roft_gpt2_generator.py
@@ -1,17 +1,26 @@
-import subprocess, json, time, argparse
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, set_seed
+'''
+RoFT Generation Script
+
+Example Usage:
+  python roft_gpt2_generator.py --dataset nyt --split test --num_gens 1000 --model_name gpt2-xl
+
+Before running this script, make sure you have run the following:
+  pip install transformers
+  pip install -U spacy
+  python -m spacy download en_core_web_trf 
+'''
+
+import subprocess, json, time, argparse, os, spacy, math
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 from tqdm.auto import tqdm
-import spacy
-from spacy.pipeline import Sentencizer
 from datetime import date
 from operator import eq
 from itertools import chain
-import math
 
-def verb_filter(generation):
-  nlp = spacy.load("en_core_web_sm")
-  for line in generation:
-    pos = [token.pos_ for token in nlp(str(line))]
+def verb_filter(nlp, generation):
+  processed_lines = list(nlp.pipe(generation))
+  for line in processed_lines:
+    pos = [token.pos_ for token in line]
     if "VERB" not in pos and "AUX" not in pos:
       return True
   return False
@@ -22,9 +31,6 @@ def cutoff_prompt(prompt, generation):
   prompt_cutoff = prompt[int(len(prompt)*0.9):]
   start = generation.find(prompt_cutoff)
   if start == -1:
-    # print("Error: couldn't find the substring!")
-    # print(repr(prompt))
-    # print(repr(generation))
     return generation[start+len(prompt):]
   return generation[start+len(prompt_cutoff):]
 
@@ -68,120 +74,106 @@ if args.vary_p and args.num_gens < 11 * MIN_NUM_SENTS:
   print("Warning: Please set args.num_gens higher, unable to make equal spread across p values and prompt lengths")
   exit(0)
 
-# Download the sampling file from the Google Cloud bucket
+# Get the name of the prompts json file
 filename = 'prompts-{}.json'.format('-'.join([args.dataset, args.split]))
-file_url = 'gs://roft_datasets/prompts/' + args.dataset + '/' + filename
 local_file_path = './' + args.dataset + '/' + filename
-command = "gsutil cp {0} {1}".format(file_url, local_file_path)
-process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+file_url = 'gs://roft_datasets/prompts/' + args.dataset + '/' + filename
+
+# If we do not have the file already, download from gcloud bucket
+if not os.path.exists(local_file_path):
+  command = "gsutil cp {0} {1}".format(file_url, local_file_path)
+  process = subprocess.Popen(command.split(), stdout=subprocess.PIPE).wait()
 
 # Initialize the tokenizer and the model
-tokenizer = GPT2Tokenizer.from_pretrained(args.model_name)
-model = GPT2LMHeadModel.from_pretrained(args.model_name, return_dict=True).cuda()
-
-# Calculate the 99 percentile length of a prompt with MIN_NUM_SENTS sentences in it
-with open(local_file_path, 'r') as f:
-  data = json.load(f)
-  tokenized_prompts = []
-  for prompt in data['prompts']:
-    inputs = tokenizer(' '.join(prompt[:MIN_NUM_SENTS]), return_tensors="pt")
-    tokenized_prompts.append(float(len(inputs['input_ids'][0])) / float(MIN_NUM_SENTS))
-sorted_lens = sorted(tokenized_prompts)
-ninety_nine_percentile_sent_len = sorted_lens[int(0.99*len(sorted_lens))]
-
-nlp = spacy.load("en")
-
+tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+model = AutoModelForCausalLM.from_pretrained(args.model_name, return_dict=True).cuda()
 set_seed(RANDOM_SEED)
 
-generations = []
-failure_causes = [0] * 6
+spacy.prefer_gpu()
+nlp = spacy.load("en_core_web_trf", exclude=['ner', 'lemmatizer'])
+
+# Parse the json file into a dict
 with open(local_file_path, 'r') as f:
   data = json.load(f)
 
-  # Calculate some important values
-  num_gens = len(data['prompts'][:args.num_gens])
-  prompts_per_length = num_gens / MIN_NUM_SENTS
+# Make sure num_gens isn't larger than the total number of prompts in the file
+num_gens = min(args.num_gens, len(data['prompts']))
+prompts_per_length = num_gens / MIN_NUM_SENTS
+generations = []
+failure_causes = [0] * 6
 
-  # For each batch of generations
-  for i in tqdm(range(0, num_gens)):
+# For each batch of generations
+for i in tqdm(range(0, num_gens)):
 
-    # Calculate prompt_length and p_value deterministically from index
-    prompt_length = int((i / prompts_per_length) + 1)
-    p_value = round(math.floor(float(float(i%prompts_per_length) / (float(prompts_per_length) / 11.0))) / 10.0, 1) if args.vary_p else 0.4
+  # Calculate prompt_length and p_value deterministically from index
+  prompt_length = int((i / prompts_per_length) + 1)
+  p_value = round(math.floor(float(float(i%prompts_per_length) / (float(prompts_per_length) / 11.0))) / 10.0, 1) if args.vary_p else 0.4
 
-    # If the prompt isn't long enough to support the prompt_length we want, continue
-    if len(data['prompts'][i]) < prompt_length:
-      failure_causes[5] += 1
-      continue
+  # If the prompt isn't long enough to support the prompt_length we want, skip it
+  if len(data['prompts'][i]) < prompt_length:
+    failure_causes[5] += 1
+    continue
 
-    # Sample and tokenize the prompts for this batch
-    prompt = data['prompts'][i][:prompt_length]
-    inputs = tokenizer.encode(' '.join(prompt), return_tensors="pt")
+  # Sample and tokenize the prompt
+  prompt = data['prompts'][i][:prompt_length]
+  inputs = tokenizer.encode(' '.join(prompt), return_tensors="pt", truncation=True)
 
-    # Calculate the max_length for this prompt using the 90th percentile sentence length in the corpus
-    longest_prompt_len = max([len(ids) for ids in inputs])
-    max_len = longest_prompt_len + int((MIN_NUM_SENTS - prompt_length) * ninety_nine_percentile_sent_len)
+  if prompt_length >= MIN_NUM_SENTS:
+    generations.append({'prompt': prompt, 'generation': [], 'p': p_value, 'prompt-index': i})
+    continue
 
-    if longest_prompt_len >= 1024:
-      failure_causes[0] += 1
-      continue
+  # Generate the outputs
+  output_sequences = model.generate(
+    inputs.to(model.device),
+    do_sample=True,
+    top_p=min(p_value,1.0),
+    repetition_penalty=1.2,
+    pad_token_id=tokenizer.eos_token_id,
+    max_length=1024,
+    min_length=1024
+  )
 
-    if prompt_length >= MIN_NUM_SENTS:
-      generations.append({'prompt': prompt, 'generation': [], 'p': p_value, 'prompt-index': i})
-      continue
+  # Decode the batched outputs (making sure to skip the special padding token)
+  outputs = [tokenizer.decode(x, skip_special_tokens=True) for x in output_sequences]
 
-    # Generate the outputs
-    output_sequences = model.generate(
-      inputs.to(model.device),
-      do_sample=True,
-      top_p=min(p_value,1.0),
-      top_k=0,
-      repetition_penalty=1.2,
-      pad_token_id=tokenizer.eos_token_id,
-      max_length=min(max_len, 1024)
-    )
+  generated_text = cutoff_prompt(' '.join(prompt), outputs[0]).strip()
+  processed_lines = list(nlp.pipe(generated_text.split('\n\n')))
+  generated_sents = list(chain.from_iterable([line.sents for line in processed_lines]))[len(prompt):]
+  generation = fix_quotation_marks([str(sent).replace('\n', '') for sent in generated_sents])
 
-    # Decode the batched outputs (making sure to skip the special padding token)
-    outputs = [tokenizer.decode(x, skip_special_tokens=True) for x in output_sequences]
+  # Reject all generations that don't meet the minimum sentence length requirements
+  if len(generation) + len(prompt) < MIN_NUM_SENTS:
+    failure_causes[1] += 1
+    continue
 
-    generated_text = cutoff_prompt(' '.join(prompt), outputs[0]).strip()
-    processed_lines = [nlp(line) for line in generated_text.split('\n\n')]
-    generated_sents = list(chain.from_iterable([line.sents for line in processed_lines]))[len(prompt):]
-    generation = fix_quotation_marks([str(sent).replace('\n', '') for sent in generated_sents])
+  truncated = generation[:MIN_NUM_SENTS-len(prompt)]
 
-    # Reject all generations that don't meet the minimum sentence length requirements
-    if len(generation) + len(prompt) < MIN_NUM_SENTS:
-      failure_causes[1] += 1
-      continue
+  # Reject generations with lines that are too short
+  if min([len(s) for s in truncated]) <= 3:
+    failure_causes[2] += 1
+    continue
 
-    truncated = generation[:MIN_NUM_SENTS-len(prompt)]
+  # Reject generations that are repetitive
+  if REJECT_IF_REPETITIVE and any(map(eq, truncated, truncated[1:])):
+    failure_causes[3] += 1
+    continue
 
-    # Reject generations with lines that are too short
-    if min([len(s) for s in truncated]) <= 3:
-      failure_causes[2] += 1
-      continue
+  if SPACY_VERB_FILTER and verb_filter(nlp, truncated):
+    failure_causes[4] += 1
+    continue
 
-    # Reject generations that are repetitive
-    if REJECT_IF_REPETITIVE and any(map(eq, truncated, truncated[1:])):
-      failure_causes[3] += 1
-      continue
+  if TRUNCATE: generation = truncated
 
-    if SPACY_VERB_FILTER and verb_filter(truncated):
-      failure_causes[4] += 1
-      continue
+  generations.append({'prompt': prompt, 'generation': generation, 'p': p_value, 'prompt-index': i})
 
-    if TRUNCATE: generation = truncated
-
-    generations.append({'prompt': prompt, 'generation': generation, 'p': p_value, 'prompt-index': i})
-
-  print("Failure Rate: " + str(float(sum(failure_causes)) / float(num_gens)))
-  print("Causes:")
-  print("Prompt over 256 chars: " + str(failure_causes[0]))
-  print("Generation too short: " + str(failure_causes[1]))
-  print("Line too short: " + str(failure_causes[2]))
-  print("Repetitive: " + str(failure_causes[3]))
-  print("No Verb Present: " + str(failure_causes[4]))
-  print("Prompt too short: " + str(failure_causes[5]))
+print("Failure Rate: " + str(float(sum(failure_causes)) / float(num_gens)))
+print("Causes:")
+print("Prompt over 256 chars: " + str(failure_causes[0]))
+print("Generation too short: " + str(failure_causes[1]))
+print("Line too short: " + str(failure_causes[2]))
+print("Repetitive: " + str(failure_causes[3]))
+print("No Verb Present: " + str(failure_causes[4]))
+print("Prompt too short: " + str(failure_causes[5]))
 
 # Save the prompts to the json file
 to_save = {
