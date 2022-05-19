0aebe2ccf303e5117d4eb014fd686897ce351d1e
liamdugan <ldugan@seas.upenn.edu>
2022-05-19 11:13:00 -0400

    Updated new generations

--- annotation/core/views.py
+++ annotation/core/views.py
@@ -22,7 +22,7 @@ BATCH_SIZE = 10
 # assigned to this many users before any new annotation gets assigned.
 GOAL_NUM_ANNOTATIONS = 3
 # The playlist version to show in the UI
-_PLAYLIST_VERSION = "0.2"
+_PLAYLIST_VERSION = "0.3"
 
 
 # The cached leaderboard.
